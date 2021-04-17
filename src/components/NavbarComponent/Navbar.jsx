1313ce925235781be1450b9237456f13fab175a8
Mohammad Saif <saminsaif29@gmail.com>
2021-04-17 18:48:39 -0400

    Nabar

---
+++ src/components/NavbarComponent/Navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import logo from "../../assets/mealLogo.png";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <img src={logo} alt="logo" />
+          <p>Meal Mate</p>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-end">
+          <a className="navbar-item" href="/">
+            Search
+          </a>
+          <a className="navbar-item" href="/">
+            Grocery
+          </a>
+          <a className="navbar-item" href="/">
+            Calendar
+          </a>
+          <a className="navbar-item" href="/">
+            Profile
+          </a>
+          <a className="navbar-item" href="/">
+            Logout
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
