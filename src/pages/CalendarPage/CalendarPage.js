1b14e5bfe8cd6f29f267930b44a9cd708463f886
Danish11512 <danish.faruqi1@gmail.com>
2021-04-23 17:39:37 -0400

    Calenday Day updated

--- src/pages/CalendarPage/CalendarPage.js
+++ src/pages/CalendarPage/CalendarPage.js
@@ -5,26 +5,19 @@ import CalendarView from '../../components/CalendarComponent/CalendarView'
 const CalendarPage = (props) =>{
     const [calendarId, setCalendarId] = useState(props.currentUser.calendarId)
     const [startDate, setStartDate] = useState(new Date())
-    // const [nextDate, setNextDate] = useState(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 5))
-    // const [calendarInfo, setCalendarInfo] = useState({})
 
-    // useEffect(() => {
-    //     const getCalenderInfo = async () => {
-    //         setCalendarInfo(await firebase.getCalendarDateRange(calendarId, startDate, nextDate))
-    //     }
-    //     getCalenderInfo()
-    // }, [startDate])
+    useEffect(() => {
+        setStartDate(startDate)
+        
+    }, [startDate])
 
-    // console.log(calendarInfo)
 
     function decreaseDate(event){
         setStartDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()-5))
-        // setNextDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+5))
     }
 
     function increaseDate(event){
         setStartDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+5))
-        // setNextDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+5))
     }
 
     return (
@@ -32,11 +25,11 @@ const CalendarPage = (props) =>{
             <div id="list" className="column is-2 has-background-dark">
                 style for side component 
             </div>
-            <div className="column has-background-primary">
-                <div className="column has-background-warning">
+            <div className="column">
+                <div className="column">
                     <h1 className="has-text-centered">Meal Plan</h1>
                 </div>
-                <div id="calendar" className="columns has-background-danger">
+                <div id="calendar" className="columns">
                     <div id="button-left" className="column is-narrow has-background-light">
                         <button onClick={e => decreaseDate(e)}> back </button>
                     </div>
