d7afb8e417c8cde7f45959d6dd924a378690b40e
Danish11512 <danish.faruqi1@gmail.com>
2021-04-23 02:29:17 -0400

    calendar layout done

--- src/pages/CalendarPage/CalendarPage.js
+++ src/pages/CalendarPage/CalendarPage.js
@@ -9,28 +9,18 @@ const CalendarPage = (props) =>{
     const [previousRecipes, setPreviousRecipes] = useState(null)
 
     useEffect(() => {
-        setStartDate(startDate)
+        // async function getCalendarInfo(){
+        //     calendarInfo = await firebase.getCalendarDateRange(calendarId, startDate, nextDate)
+        // }
+        // getCalendarInfo()
 
-        if(previousRecipesArray.length < 1){
-            setPreviousRecipes(
-                <div>
-                    <p className="has-text-black">
-                        <br></br><br></br><br></br><br></br><br></br>
-                        Looks like you dont have any recent meals &#129370;
-                        <br></br><br></br><br></br><br></br><br></br>
-                    </p>
-                </div>
-            )
-            
-        }else{
-            setPreviousRecipes()
-        }
-
+        // console.log(calendarInfo)
+        // setNextDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+5))
     }, [startDate])
 
-
     function decreaseDate(event){
-        setStartDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()-6))
+        setStartDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()-5))
+        setNextDate(new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate()+5))
     }
 
     function increaseDate(event){
@@ -39,40 +29,26 @@ const CalendarPage = (props) =>{
 
     return (
         <div className="columns">
-            <div id="list" className="column is-2">
-            <div className="title is-5 has-text-centered"> Add to Calendar</div>
-                <div className="card has-background-warning">
-                    <div className="card-content has-text-centered">
-                        <div className="field">
-                            <div className="control">
-                                <input className="input" type="text" placeholder="Recipe"></input>
-                            </div>
-                        </div>
-                    <div className="subtitle is-6 has-text-centered"> Satisfy your cravings</div>
-                    <br></br><br></br><br></br><br></br><br></br><br></br><br></br>
-                    </div>
-                </div>
-                <br></br>
-                <div className="title is-5 has-text-centered"> Your Recent Recipes</div>
-                <div className="card has-background-info">
-                    <div className="card-content">
-                        {previousRecipes}
-                    </div>
-                </div>
+            <div id="list" className="column is-2 has-background-dark">
+                style for side component 
             </div>
-            <div className="column">
-                <div className="column">
+            <div className="column has-background-primary">
+                <div className="column has-background-warning">
                     <h1 className="has-text-centered">Meal Plan</h1>
                 </div>
-                <div id="calendar" className="columns">
+                <div id="calendar" className="columns has-background-danger">
                     <div id="button-left" className="column is-narrow has-background-light">
-                        <button onClick={e => decreaseDate(e)}> &#8592; </button>
+                        <button onClick={e => decreaseDate(e)}> back </button>
                     </div>
                     <div className="column is-10.5">
-                         <CalendarView calendarId = {calendarId} startDate={startDate}></CalendarView>
+                        {nextDate.toString()}
+                        <br></br>
+                        <br></br>
+                        <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+                         <CalendarView calenderinfo={calendarInfo}></CalendarView>
                     </div>
                     <div id="button-right" className="column is-narrow has-background-light">
-                        <button onClick={e => increaseDate(e)}> &#8594; </button>
+                        <button onClick={e => increaseDate(e)}> forward </button>
                     </div>
                 </div>
 
