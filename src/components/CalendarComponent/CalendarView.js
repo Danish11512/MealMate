8254d274ddd780d7fa52823d0e3f9e05bc42affe
Danish11512 <danish.faruqi1@gmail.com>
2021-04-23 02:29:17 -0400

    calendar layout done yes

--- src/components/CalendarComponent/CalendarView.js
+++ src/components/CalendarComponent/CalendarView.js
@@ -1,39 +1,11 @@
 import React, { useEffect, useState } from "react";
-import * as firebase from '../../firebase/firebase.utils';
-import CalendarDay from './CalendarDay'
-import "../../pages/CalendarPage/CalendarPage.css";
 
 const CalenderView = (props) =>{
-    const [calendarDays, setCalendarDays] = useState([])
-    let endDate = null
-    
-    useEffect(() => {
-        endDate = new Date(props.startDate.getFullYear(), props.startDate.getMonth(), props.startDate.getDate() + 5)
-        
-        const getCalenderInfo = async () => {
-            let calendarInfo = await firebase.getCalendarDateRange(props.calendarId, props.startDate, endDate)
-            setCalendarDays(Object.keys(calendarInfo).map(k=> [k, calendarInfo[k]]))
-        }
-        
-        getCalenderInfo()
-        
-    } ,[props.startDate])
-
     return(
-        <div className="columns">
-            <div className="column is-4">
-                <CalendarDay dayInfo={calendarDays[0]}></CalendarDay>
-                <CalendarDay dayInfo={calendarDays[1]}></CalendarDay>
-            </div>
-            <div className="column is-4">
-                <CalendarDay dayInfo={calendarDays[2]}></CalendarDay>
-                <CalendarDay dayInfo={calendarDays[3]}></CalendarDay>
-            </div>
-            <div className="column is-4">
-                <CalendarDay dayInfo={calendarDays[4]}></CalendarDay>
-                <CalendarDay dayInfo={calendarDays[5]}></CalendarDay>
-            </div>
-        </div>
+
+        <>
+
+        </>
     )
 }
 
