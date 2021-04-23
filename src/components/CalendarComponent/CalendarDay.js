30027422b88e1942e2816b25aec5e5feb2bc8cf8
Danish11512 <danish.faruqi1@gmail.com>
2021-04-23 17:39:37 -0400

    Calenday Day updated

--- src/components/CalendarComponent/CalendarDay.js
+++ src/components/CalendarComponent/CalendarDay.js
@@ -1,23 +1,58 @@
-import React, { useEffect, useState } from "react";
-import * as firebase from '../../firebase/firebase.utils';
-import "../../pages/CalendarPage/CalendarPage.css";
+import React, { useEffect, useState } from "react"
+import "../../pages/CalendarPage/CalendarPage.css"
+import CalendarMeal from './CalendarMeal'
 
 const CalendarDay = (props) =>{
     const [dayInfo, setDayInfo] = useState(props.dayInfo)
+    const [meals, setMeals] = useState([])
+    const [totalCalories, seTotalCalories] = useState(0)
+    const [day, setDay] = useState("")
 
     useEffect(() => {
-        setDayInfo(props.dayInfo)
+        const getDayInfo = async () => {
+            setDayInfo(await props.dayInfo)
+        }
+        getDayInfo().then(console.log(dayInfo))
+                    // .then(setDay(dayInfo[0]))
+                    .then(setMeals[1])
+            
 
+        
+        
+        // const getMeals = async () =>{
+        //     setMeals(await dayInfo[1])
+        // }
+
+        // getMeals()
+        
+        
         
     }, [props.dayInfo])
 
-    console.log(dayInfo)
-
-
-
-
     return(
-        <div className="">
+        <div>
+        <div className="card mb-6">
+            <header className="card-header">
+                <p className="card-header-title">
+                Date
+                </p>
+            </header>
+
+            <div className="card-content">
+                <div className="content">
+                    Meal
+                    <br></br>
+                    meal
+                    <br></br>
+                    Meal
+                </div>
+                {/* <CalendarMeal></CalendarMeal> */}
+            </div>
+
+            <footer className="card-footer">
+                Total Calories: {totalCalories}
+            </footer>
+        </div>
         </div>
     )
 }
