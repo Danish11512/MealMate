a558a39409f2863dfbb43aa454ec9931b512d835
fzaman2258 <fzaman2258@bths.edu>
2021-04-29 16:22:10 -0400

    Added Survey Drop Down

---
+++ src/components/ProfileComponent/Survey.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Survey.css";
+
+<Collapsible trigger = "Review Survey Information" classParentString = "survey_info" >
+                       {/* 
+                            It should be a dropdown to specify their diet and then a bunch of checkboxes for common intolerances
+                       */}
+                       
+
+                            
+
+
+</Collapsible>
