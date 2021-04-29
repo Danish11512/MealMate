154a435c46bf5c008f7a9ac494ac1571ea3caf6b
Arman <uarman2215@gmail.com>
2021-04-29 13:46:58 -0400

    Added Recipe Details Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignupPage/SignupPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
 import ResultsPage from "./pages/ResultPage/SearchResultPage";
+import RecipeDetailsPage from "./pages/RecipeDetailsPage/RecipeDetailsPage";
 
 const App = () => {
   const [currentUser, setCurrentUser] = useState(null);
@@ -48,6 +49,10 @@ const App = () => {
     <div className="App">
       <Navbar currentUser={currentUser} />
       <Switch>
+        <Route
+          path="/recipe/:id"
+          render={() => currentUser ? (<RecipeDetailsPage currentUser={currentUser} />) : (<Redirect to="/" />)}
+        />
         <Route
           path="/login"
           render={() => (currentUser ? <Redirect to="/" /> : <LoginPage />)}
