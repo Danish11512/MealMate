8abebd3ead304beed2707f7db937b91b84d6445f
Danish11512 <danish.faruqi1@gmail.com>
2021-05-01 11:39:50 -0400

    faster load of data

--- src/components/CalendarComponent/CalendarMeal.js
+++ src/components/CalendarComponent/CalendarMeal.js
@@ -1,20 +1,37 @@
 import React, { useEffect, useState } from "react"
 import "../../pages/CalendarPage/CalendarPage.css"
 
-const CalendarMeal = async (props) =>{
-    const [meal, setMeal] = useState([])
+const CalendarMeal = async (key, props) =>{
+    let mealId = null
+    let date = null
+    let recipeId = null
+    let recipeName = null
+    let time = null
 
+    useEffect(() => {
+        mealId = key.mealId
+        date = props.date
+        recipeId = props.recipeId
+        recipeName = props.recipeName
+        time = props.time
+        
+    }, [props, key])
+    
 
     return(
-        <div className="card">
-            <div className="card-content">
-                <div className="modal">
-                    <div className="modal-content">
-                    </div>
-                    <div className="modal-content">
-                    </div>
-                </div>
-            </div>
+        // <div className="card">
+        //     <div className="card-content">
+        //         <div className="modal">
+        //             <div className="modal-content">
+        //             <p className="has-text-black">hi</p>
+        //             </div>
+        //             <div className="modal-content">
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div>
+            {mealId}
         </div>
     )
 
