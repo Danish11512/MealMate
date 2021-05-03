8edd8cba2b4a7a47059d2895521c6b3af8da128f
Maya Pasiliao <mayapasiliao@gmail.com>
2021-05-03 14:27:06 -0400

    Fixed api cycling

--- src/spoonacular.utils.js
+++ src/spoonacular.utils.js
@@ -1,7 +1,7 @@
 //import axios from 'axios';
 
 const fetch = require("node-fetch");
-const SPOONACULAR_API_KEYS = ["c596eb18f99d49ea8d8895ef5f10840d", "46aecb3dc8b54eedaa242d3ca5a6b33d", "6a37f4e32e674edf86796454ce8dc8b7", "46aecb3dc8b54eedaa242d3ca5a6b33d"];
+const SPOONACULAR_API_KEYS = ["c596eb18f99d49ea8d8895ef5f10840d", "46aecb3dc8b54eedaa242d3ca5a6b33d", "6a37f4e32e674edf86796454ce8dc8b7", "baa7a2e369eb42599d83a5e79692bb15"];
 
 // Gets recipe from spoonacular using id
 // Takes: Id as string or numeric type, up to you bb <3
@@ -17,8 +17,11 @@ export const getRecipeById = async (id) => {
 			let response_json = await response.json(); // Doesn't return json object, returns js object
 			return response_json;
 		} else if (response.status === 402) { // If first chose api key is at its point limit try every single api key until one works
+			let currentKey = SPOONACULAR_API_KEYS[randomNum];
+
 			for (const key of SPOONACULAR_API_KEYS) {
-				queryString = queryString.replace(SPOONACULAR_API_KEYS[randomNum], key);
+				queryString = queryString.replace(currentKey, key);
+				currentKey = key;
 
 				let response2 = await fetch(queryString, { method: "GET" });
 				if (response2.status === 200) {
@@ -82,7 +85,7 @@ export const searchRecipe = async (searchQuery, filters) => {
 				type = "salad,bread,soup,fingerfood,appetizer";
 			} else if (
 				filters["mealType"] === "lunch" ||
-        filters["mealType"] === "dinner"
+				filters["mealType"] === "dinner"
 			) {
 				type = "main%20course,soup,salad";
 			}
@@ -104,8 +107,11 @@ export const searchRecipe = async (searchQuery, filters) => {
 			let response_json = await response.json();
 			return response_json;
 		} else if (response.status === 402) { // If first chose api key is at its point limit try every single api key until one works
+			let currentKey = SPOONACULAR_API_KEYS[randomNum];
+
 			for (const key of SPOONACULAR_API_KEYS) {
-				queryString = queryString.replace(SPOONACULAR_API_KEYS[randomNum], key);
+				queryString = queryString.replace(currentKey, key);
+				currentKey = key;
 
 				let response2 = await fetch(queryString, { method: "GET" });
 				if (response2.status === 200) {
@@ -137,8 +143,11 @@ export const searchRecipeByCuisine = async (cuisine) => {
 			let response_json = await response.json();
 			return response_json;
 		} else if (response.status === 402) { // If first chose api key is at its point limit try every single api key until one works
+			let currentKey = SPOONACULAR_API_KEYS[randomNum];
+
 			for (const key of SPOONACULAR_API_KEYS) {
-				queryString = queryString.replace(SPOONACULAR_API_KEYS[randomNum], key);
+				queryString = queryString.replace(currentKey, key);
+				currentKey = key;
 
 				let response2 = await fetch(queryString, { method: "GET" });
 				if (response2.status === 200) {
