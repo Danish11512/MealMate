67c6c540e1431d9019ade75473bf7562adf5b9fd
Danish11512 <danish.faruqi1@gmail.com>
2021-05-04 01:06:23 -0400

    calendar page modal and design skeleton gh

--- src/components/CalendarComponent/CalendarMeal.js
+++ src/components/CalendarComponent/CalendarMeal.js
@@ -3,55 +3,70 @@ import "../../pages/CalendarPage/CalendarPage.css"
 import * as firebase from "../../firebase/firebase.utils"
 
 const CalendarMeal = (props) =>{
-    // let mealId = props.mealId | null
-    // let date = props.date | null
-    // let recipeId = props.recipeId | null
-    // let recipeName = props.recipeId | null
-    // let time = props.time | null
+    let mealId = props.meal.mealId
+    let date = props.meal.date
+    let recipeId = props.meal.recipeId
+    let recipeName = props.meal.recipeName
+    let time = props.meal.time
 
-    // useEffect(() => {
-        // mealId = key.mealId
-        // date = props.date
-        // recipeId = props.recipeId
-        // recipeName = props.recipeName
-        // time = props.time
+    useEffect(() => {
+        mealId = props.meal.mealId
+        date = props.meal.date
+        recipeId = props.meal.recipeId
+        recipeName = props.meal.recipeName
+        time = props.meal.time
+
+
+		const mealBox = document.querySelector(".box")
+		const modal = document.querySelector(".modal")
+		const modalBg = document.querySelector(".modal-background")
+
+		mealBox.addEventListener("click", () => {
+			modal.classList.add("is-active")
+		})
+
+		modalBg.addEventListener("click", () => {
+			modal.classList.remove("is-active")
+		})
         
-    // }, [])
+    }, [props])
     
+
+
+	
+
+
 
     return(
         <div>
-            <div onClick={openModal} className="box">
+            <div className="box">
                 <div>
                     <div className="has-text-left">{recipeName}</div>
                     <div className="has-text-right">{time}</div>
                 </div>
 			</div>
-
-            <div className={`modal ${modal}`}>
-                <div onClick={closeModal} className="modal-background"></div>
-                <div className="modal-content has-background-white">
-                    <div className="box">
-                        <article className="media">
-                            <div className="media-left">
-                                <figure className="image is-128x128">
-                                    <img src={recipe.image} alt="Image"></img>
-                                </figure>
-                            </div>
-                            <div className="media-content">
-                                <div className="content">
-                                    <h3>{recipeName}</h3>
-                                    <h5>{time}</h5>
-                                    <p>	
-                                        <a>{recipe.sourceUrl}</a>
-                                    </p>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-            
+			<div className="modal">
+				<div className="modal-background"></div>
+				<div className="modal-content has-background-white">
+					<div class="box">
+  						<article class="media">
+							<div class="media-left">
+								<figure class="image is-256x256">
+									<img src="https://bulma.io/images/placeholders/256x256.png" alt="Image"></img>
+								</figure>
+							</div>
+							<div class="media-content">
+								<div class="content">
+									<p>
+										<h3>{recipeName}</h3>
+									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+									</p>
+								</div>
+    						</div>
+  						</article>
+					</div>
+				</div>
+			</div>
         </div>  
     )
 
