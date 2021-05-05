08ab2f840b64dc36a51147878e85944e6506c399
Danish11512 <danish.faruqi1@gmail.com>
2021-05-05 14:47:39 -0400

    everything but modal done on list

--- src/components/CalendarComponent/CalendarMeal.js
+++ src/components/CalendarComponent/CalendarMeal.js
@@ -23,51 +23,18 @@ const CalendarMeal = (props) =>{
 
 		getRecipeData()
 		
-		// const mealBox = document.querySelector(".box")
-		// const modal = document.querySelector(".modal")
-		// const modalBg = document.querySelector(".modal-background")
-
-		// mealBox.addEventListener("click", () => {
-		// 	modal.classList.add("is-active")
-		// })
-
-		// modalBg.addEventListener("click", () => {
-		// 	modal.classList.remove("is-active")
-		// })
         
     }, [props.meal])
 
     return(
         <div>
-            <div className="box">
+            <div className="box py-3">
                 <div>
                     <div className="has-text-left">{recipeName}</div>
                     <div className="has-text-right">{time}</div>
                 </div>
 			</div>
-			{/* <div className="modal">
-				<div className="modal-background"></div>
-				<div className="modal-content has-background-white">
-					<div className="box">
-  						<article className="media">
-							<div className="media-left">
-								<figure className="image is-256x256">
-									<img src="https://bulma.io/images/placeholders/256x256.png" alt="Image"></img>
-								</figure>
-							</div>
-							<div className="media-content">
-								<div className="content">
-									<h3>{recipeName}</h3>
-									<h5>{time}</h5>
-									<p>	
-									Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
-									</p>
-								</div>
-    						</div>
-  						</article>
-					</div>
-				</div>
-			</div> */}
+			
         </div>  
     )
 
